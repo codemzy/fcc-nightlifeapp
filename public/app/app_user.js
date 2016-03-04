a2e763514606101e61ddbefa1030efb579f0b3d5
Emma J <codemzy@gmail.com>
2016-03-04 11:09:29 +0000

    Finished login and profile pages, and passport functionality

---
+++ public/app/app_user.js
@@ -0,0 +1,19 @@
+angular.module('OwlBeThereApp', ['ngRoute'])
+
+// app routes
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+    templateUrl: '/public/app/views/home.html'
+    })
+    // logged in routes
+    .when('/profile', {
+    templateUrl: '/public/app/views/user_profile.html',
+    controller: 'ProfileController'
+    })
+    // default
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+
+});
